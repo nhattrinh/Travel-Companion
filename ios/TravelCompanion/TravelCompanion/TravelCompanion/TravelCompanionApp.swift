c4bdde9dcf6047bae8d9a08847f1c7b50f6429be
Nhat Trinh <nhtrinh@cisco.com>
2025-11-28 22:00:28 -0800

    Fix build errors for Swift project, backend errors, and fix docker startup. End-to-end works

---
+++ ios/TravelCompanion/TravelCompanion/TravelCompanion/TravelCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCompanionApp.swift
+//  TravelCompanion
+//
+//  Created by Nhat Trinh on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
